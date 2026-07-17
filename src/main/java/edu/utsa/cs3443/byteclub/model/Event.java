82655ffe06fe75874ae094b3ee9f28ba9107a9f7
Dylan <dylanjmsguzman@gmail.com>
2026-07-16 22:26:14 -0500

    fixed event / item java file placement?

--- src/main/java/edu/utsa/cs3443/byteclub/model/Event.java
+++ src/main/java/edu/utsa/cs3443/byteclub/model/Event.java
@@ -1,4 +1,11 @@
-package edu.utsa.cs3443.byteclub.model;
+package edu.utsa.cs3443.byteclub;
 
 public class Event {
+
+    String name;
+    String dateAndTime; //use Java time class
+    String location;
+    String instructor;
+    int numAttendees;
+
 }
