c3609df1b246673c2fa7b06b0a6c445aa5ec24a3
RodgersR2098 <162671408+RRodgers2098@users.noreply.github.com>
2026-07-16 22:59:34 -0500

    Getters and Setters for Item and Event
    Getters and setters and event constructor

--- src/main/java/edu/utsa/cs3443/byteclub/model/Event.java
+++ src/main/java/edu/utsa/cs3443/byteclub/model/Event.java
@@ -2,10 +2,84 @@ package edu.utsa.cs3443.byteclub;
 
 public class Event {
 
-    String name;
-    String dateAndTime; //use Java time class
-    String location;
-    String instructor;
-    int numAttendees;
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getDateAndTime() {
+        return dateAndTime;
+    }
+
+    public void setDateAndTime(String dateAndTime) {
+        this.dateAndTime = dateAndTime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public int getNumSeats() {
+        return numSeats;
+    }
+
+    public void setNumSeats(int numSeats) {
+        this.numSeats = numSeats;
+    }
+
+    public int getNumAttendees() {
+        return numAttendees;
+    }
+
+    public void setNumAttendees(int numAttendees) {
+        this.numAttendees = numAttendees;
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    public void setOpen(boolean open) {
+        isOpen = open;
+    }
+
+    private String courseName;
+    private String dateAndTime; //use Java time class
+    private String location;
+    private String instructor;
+    private int numSeats;
+    private int numAttendees;
+    private boolean isOpen;
+
+    Event(String courseName, String dateAndTime, String location, String instructor, int numSeats, int numAttendees, boolean isOpen) {
+        this.courseName = courseName;
+        this.dateAndTime = dateAndTime;
+        this.location = location;
+        this.instructor = instructor; //actually I think we can have this just be the name from instructor/person class     instructor.getName
+        this.numSeats = numSeats;
+        this.numAttendees = numAttendees;
+        this.isOpen = isOpen;
+
+    }
+
+
+
+    //method that if numAttendees >= numSeats, display a message saying cannot join class
+
 
 }
--- src/main/java/edu/utsa/cs3443/byteclub/model/Item.java
+++ src/main/java/edu/utsa/cs3443/byteclub/model/Item.java
@@ -2,9 +2,42 @@ package edu.utsa.cs3443.byteclub;
 
 public class Item {
     String name;
-    double price;
-    boolean inStock;
-    boolean hasNutritionalValue;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isInStock() {
+        return inStock;
+    }
+
+    public void setInStock(boolean inStock) {
+        this.inStock = inStock;
+    }
+
+    public boolean isHasNutritionalValue() {
+        return hasNutritionalValue;
+    }
+
+    public void setHasNutritionalValue(boolean hasNutritionalValue) {
+        this.hasNutritionalValue = hasNutritionalValue;
+    }
+
+    private double price;
+    private boolean inStock;
+    private boolean hasNutritionalValue;
 
     Item(String name, double price, boolean inStock, boolean hasNutritionalValue) {
         this.name = name;
