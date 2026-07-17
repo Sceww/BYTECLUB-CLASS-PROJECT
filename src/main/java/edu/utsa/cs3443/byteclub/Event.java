fa871919c0c52f4a0e066da43e552532663587f5
RodgersR2098 <162671408+RRodgers2098@users.noreply.github.com>
2026-07-16 22:04:59 -0500

    Creating classes
    Added skeleton of classes and removed test file

---
+++ src/main/java/edu/utsa/cs3443/byteclub/Event.java
@@ -0,0 +1,4 @@
+package edu.utsa.cs3443.byteclub;
+
+public class Event {
+}
---
+++ src/main/java/edu/utsa/cs3443/byteclub/Person.java
@@ -0,0 +1,8 @@
+package edu.utsa.cs3443.byteclub;
+
+public class Person {
+    String firstName;
+    String lastName;
+
+
+}
