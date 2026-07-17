ec8e858c5b0c7a4330114853b4a8e4e0e9e7becb
RodgersR2098 <162671408+RRodgers2098@users.noreply.github.com>
2026-07-16 22:16:54 -0500

    Skeleton for Event and Item
    Ahh, skeletons

--- src/main/java/edu/utsa/cs3443/byteclub/Item.java
+++ src/main/java/edu/utsa/cs3443/byteclub/Item.java
@@ -1,4 +1,19 @@
 package edu.utsa.cs3443.byteclub;
 
 public class Item {
+    String name;
+    double price;
+    boolean inStock;
+    boolean hasNutritionalValue;
+
+    Item(String name, double price, boolean inStock, boolean hasNutritionalValue) {
+        this.name = name;
+        this.price = price;
+        this.inStock = inStock;
+        this.hasNutritionalValue = hasNutritionalValue;
+
+    }
+
+
+
 }
