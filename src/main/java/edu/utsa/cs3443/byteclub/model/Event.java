86aae50d041200d769173da75afd356e31287cf9
RodgersR2098 <162671408+RRodgers2098@users.noreply.github.com>
2026-07-22 12:32:41 -0500

    Item and Event updates
    Cleaner getter/setter, Event class now has canJoin method and Item class now has an enum to categorize items

--- src/main/java/edu/utsa/cs3443/byteclub/model/Event.java
+++ src/main/java/edu/utsa/cs3443/byteclub/model/Event.java
@@ -1,72 +1,41 @@
-package edu.utsa.cs3443.byteclub;
+package edu.utsa.cs3443.byteclub.model;
+
+import edu.utsa.cs3443.byteclub.model.Person.User;
 
 public class Event {
 
-    public String getCourseName() {
-        return courseName;
-    }
+    public String getCourseName() { return courseName; }
+    public void setCourseName(String courseName) { this.courseName = courseName;}
 
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
+    public String getDateAndTime() { return dateAndTime; }
+    public void setDateAndTime(String dateAndTime) { this.dateAndTime = dateAndTime; }
 
-    public String getDateAndTime() {
-        return dateAndTime;
-    }
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
 
-    public void setDateAndTime(String dateAndTime) {
-        this.dateAndTime = dateAndTime;
-    }
+    public User getInstructor() { return instructor; }
+    public void setInstructor(User instructor) { this.instructor = instructor; }
 
-    public String getLocation() {
-        return location;
-    }
+    public String getInstructorName() { return instructor.getFirstName() + " " + instructor.getLastName(); }
 
-    public void setLocation(String location) {
-        this.location = location;
-    }
+    public int getNumSeats() { return numSeats; }
+    public void setNumSeats(int numSeats) { this.numSeats = numSeats; }
 
-    public String getInstructor() {
-        return instructor;
-    }
+    public int getNumAttendees() { return numAttendees; }
+    public void setNumAttendees(int numAttendees) { this.numAttendees = numAttendees; }
 
-    public void setInstructor(String instructor) {
-        this.instructor = instructor;
-    }
-
-    public int getNumSeats() {
-        return numSeats;
-    }
-
-    public void setNumSeats(int numSeats) {
-        this.numSeats = numSeats;
-    }
-
-    public int getNumAttendees() {
-        return numAttendees;
-    }
-
-    public void setNumAttendees(int numAttendees) {
-        this.numAttendees = numAttendees;
-    }
-
-    public boolean isOpen() {
-        return isOpen;
-    }
-
-    public void setOpen(boolean open) {
-        isOpen = open;
-    }
+    public boolean isOpen() { return isOpen; }
+    public void setOpen(boolean open) { isOpen = open; }
 
     private String courseName;
     private String dateAndTime; //use Java time class
     private String location;
-    private String instructor;
+    private User instructor;
     private int numSeats;
     private int numAttendees;
     private boolean isOpen;
 
-    Event(String courseName, String dateAndTime, String location, String instructor, int numSeats, int numAttendees, boolean isOpen) {
+    public Event(String courseName, String dateAndTime, String location, User instructor, int numSeats, int numAttendees, boolean isOpen) {
         this.courseName = courseName;
         this.dateAndTime = dateAndTime;
         this.location = location;
@@ -78,8 +47,10 @@ public class Event {
     }
 
 
+    public boolean canJoin() {
+        return isOpen && numAttendees < numSeats;
+    }
 
-    //method that if numAttendees >= numSeats, display a message saying cannot join class
 
 
 }
--- src/main/java/edu/utsa/cs3443/byteclub/model/Item.java
+++ src/main/java/edu/utsa/cs3443/byteclub/model/Item.java
@@ -1,49 +1,33 @@
-package edu.utsa.cs3443.byteclub;
+package edu.utsa.cs3443.byteclub.model;
 
 public class Item {
-    String name;
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
 
-    public double getPrice() {
-        return price;
-    }
+    public boolean isInStock() { return inStock; }
+    public void setInStock(boolean inStock) { this.inStock = inStock; }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
+    public boolean isHasNutritionalValue() { return hasNutritionalValue; }
+    public void setHasNutritionalValue(boolean hasNutritionalValue) { this.hasNutritionalValue = hasNutritionalValue; }
 
-    public boolean isInStock() {
-        return inStock;
-    }
+    public enum Category { BOXING, SUPPLEMENTS, APPAREL }
 
-    public void setInStock(boolean inStock) {
-        this.inStock = inStock;
-    }
-
-    public boolean isHasNutritionalValue() {
-        return hasNutritionalValue;
-    }
-
-    public void setHasNutritionalValue(boolean hasNutritionalValue) {
-        this.hasNutritionalValue = hasNutritionalValue;
-    }
-
+    private String name;
     private double price;
     private boolean inStock;
     private boolean hasNutritionalValue;
+    private Category category;
 
-    Item(String name, double price, boolean inStock, boolean hasNutritionalValue) {
+    public Item(String name, double price, boolean inStock, boolean hasNutritionalValue, Category category) {
         this.name = name;
         this.price = price;
         this.inStock = inStock;
         this.hasNutritionalValue = hasNutritionalValue;
+        this.category = category;
 
     }
 
