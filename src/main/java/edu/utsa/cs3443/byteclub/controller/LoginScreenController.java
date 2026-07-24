f3ff0171f02f5e9aa49695c2886e0427d6e184cd
Dylan <dylanjmsguzman@gmail.com>
2026-07-23 21:17:55 -0500

    skeleton controllers

---
+++ src/main/java/edu/utsa/cs3443/byteclub/controller/LoginScreenController.java
@@ -0,0 +1,5 @@
+package edu.utsa.cs3443.byteclub.controller;
+
+public class LoginScreenController {
+
+}
---
+++ src/main/java/edu/utsa/cs3443/byteclub/view/SceneSwitcher.java
@@ -0,0 +1,5 @@
+package edu.utsa.cs3443.byteclub.view;
+
+public class SceneSwitcher {
+
+}
